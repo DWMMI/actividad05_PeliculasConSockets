d8ba6e243df87b193fba92db7224cd96fc0681b4
D1CH40 W4N9 <dichaowang107@gmail.com>
2023-12-04 21:22:21 +0100

    modificacion en clase cliente

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -14,9 +14,9 @@ public class Cliente {
     //Puerto
     public static final int PUERTO = 2023;
     //IP del servidor
-    public static final String IP_SERVER = "localhost";
+    public static final String IP_SERVER = "192.168.1.130";
     private static int numero;
-
+    private static boolean seguir = true;
 
 
     public static void main(String[] args) {
@@ -40,56 +40,59 @@ public class Cliente {
                     + " por el puerto " + PUERTO);
             //Creamos el objeto que nos permite mandar informacion al servidor
             PrintStream salida = new PrintStream(socketAlServidor.getOutputStream());
-            //Mostrar el menu
-            System.out.println("MENU:");
-            System.out.println("1. Consultar peliculas por ID");
-            System.out.println("2. Consultar peliculas por titulo");
-            System.out.println("3. Consultar peliculas por director");
-            System.out.println("4. Agregar pelicula");
-            System.out.println("5. Salir de la aplicacion");
-            //Pedimos al usuario que elija una opcion
-            do {
-                System.out.println("CLIENTE: elige una opcion introduciendo un número de 1 - 5: ");
-                numero = Integer.parseInt(sc.nextLine());
-            } while (numero < 1 || numero > 5);
-            switch (numero) {
-                case 1:
-                    System.out.println("CLIENTE: Introduzca el ID de la pelicula");
-                    String id = sc.nextLine();
-                    String opcion = "1-" + id;
-                    mandarPeticion(opcion, direccionServidor, socketAlServidor);
-                    recibirRespuesta(socketAlServidor);
-                    break;
-                case 2:
-                    System.out.println("CLIENTE: Introduzca el titulo de la pelicula");
-                    String titulo = sc.nextLine();
-                    String opcion2 = "2-" + titulo;
-                    mandarPeticion(opcion2, direccionServidor, socketAlServidor);
-                    recibirRespuesta(socketAlServidor);
-                    break;
-                case 3:
-                    System.out.println("CLIENTE: Introduzca el director de la pelicula");
-                    String director = sc.nextLine();
-                    String opcion3 = "3-" + director;
-                    mandarPeticion(opcion3, direccionServidor, socketAlServidor);
-                    recibirRespuesta(socketAlServidor);
-                    break;
-                case 4:
-                    System.out.println("CLIENTE: Introduzca el titulo de la pelicula");
-                    String titulo2 = sc.nextLine();
-                    System.out.println("CLIENTE: Introduzca el director de la pelicula");
-                    String director2 = sc.nextLine();
-                    System.out.println("CLIENTE: Introduzca el año de la pelicula");
-                    String anio = sc.nextLine();
-                    String opcion4 = "4-" + titulo2 + "-" + director2 + "-" + anio;
-                    mandarPeticion(opcion4, direccionServidor, socketAlServidor);
-                    recibirRespuesta(socketAlServidor);
-                    break;
-                case 5:
-                    System.out.println("CLIENTE: Saliendo de la aplicacion");
-                    mandarPeticion("FIN", direccionServidor, socketAlServidor);
-                    recibirRespuesta(socketAlServidor);
-                    break;
+            while (seguir == true) {
+                //Mostrar el menu
+                System.out.println("MENU:");
+                System.out.println("1. Consultar peliculas por ID");
+                System.out.println("2. Consultar peliculas por titulo");
+                System.out.println("3. Consultar peliculas por director");
+                System.out.println("4. Agregar pelicula");
+                System.out.println("5. Salir de la aplicacion");
+                //Pedimos al usuario que elija una opcion
+                do {
+                    System.out.println("CLIENTE: elige una opcion introduciendo un número de 1 - 5: ");
+                    numero = Integer.parseInt(sc.nextLine());
+                } while (numero < 1 || numero > 5);
+                switch (numero) {
+                    case 1:
+                        System.out.println("CLIENTE: Introduzca el ID de la pelicula");
+                        String id = sc.nextLine();
+                        String opcion = "1-" + id;
+                        mandarPeticion(opcion, direccionServidor, socketAlServidor);
+                        recibirRespuesta(socketAlServidor);
+                        break;
+                    case 2:
+                        System.out.println("CLIENTE: Introduzca el titulo de la pelicula");
+                        String titulo = sc.nextLine();
+                        String opcion2 = "2-" + titulo;
+                        mandarPeticion(opcion2, direccionServidor, socketAlServidor);
+                        recibirRespuesta(socketAlServidor);
+                        break;
+                    case 3:
+                        System.out.println("CLIENTE: Introduzca el director de la pelicula");
+                        String director = sc.nextLine();
+                        String opcion3 = "3-" + director;
+                        mandarPeticion(opcion3, direccionServidor, socketAlServidor);
+                        recibirRespuesta(socketAlServidor);
+                        break;
+                    case 4:
+                        System.out.println("CLIENTE: Introduzca el titulo de la pelicula");
+                        String titulo2 = sc.nextLine();
+                        System.out.println("CLIENTE: Introduzca el director de la pelicula");
+                        String director2 = sc.nextLine();
+                        System.out.println("CLIENTE: Introduzca el año de la pelicula");
+                        String anio = sc.nextLine();
+                        String opcion4 = "4-" + titulo2 + "-" + director2 + "-" + anio;
+                        mandarPeticion(opcion4, direccionServidor, socketAlServidor);
+                        recibirRespuesta(socketAlServidor);
+                        break;
+                    case 5:
+                        System.out.println("CLIENTE: Saliendo de la aplicacion");
+                        mandarPeticion("FIN", direccionServidor, socketAlServidor);
+                        recibirRespuesta(socketAlServidor);
+                        seguir = false;
+                        break;
+                }
             }
 
 
